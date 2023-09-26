83fa09187d5cc655bb351764f58b390d8e1eb534
chrispaganon <info@chrispaganon.com>
2023-09-26 10:29:18 -0400

    Add ecosystem.config.js to gitignore

---
+++ opt/vueai.tools/ecosystem.config.sample.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'VueAI.tools',
+      port: '3002',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs',
+      env: {
+        "NUXT_OPENAI_ORGANIZATION": "org-ab12",
+        "NUXT_OPENAI_API_KEY": "sk-cd34"
+      }
+    }
+  ]
+}
